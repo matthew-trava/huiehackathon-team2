e875fb212957991243104c62a865708e4242e910
matthew-trava <matthewgtravaglini@gmail.com>
2022-04-27 20:39:27 +1000

    sentiment updates

--- nlp/sentiment_functions.py
+++ nlp/sentiment_functions.py
@@ -1,6 +1,9 @@
 import spacy
 from spacytextblob.spacytextblob import SpacyTextBlob
 import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+sent_obj = SentimentIntensityAnalyzer()
 
 nlp = spacy.load('en_core_web_md')
 nlp.add_pipe('spacytextblob')
@@ -17,7 +20,18 @@ def sentiment_groups(value):
     elif value <= 0.4:
         return "Neutral"
     else:
-        return "Postiive"
+        return "Positive"
+    
+def get_vader_sentiment(string):
+    return sent_obj.polarity_scores(string)['compound']
+
+def sentiment_groups(value):
+    if value <= -0.25:
+        return "Negative"
+    elif value <= 0.25:
+        return "Neutral"
+    else:
+        return "Positive"
     
 def get_subjectivity(string):
     doc = nlp(string)
@@ -29,6 +43,11 @@ def subjectivity_groups(value):
     else:
         return "More Objective"
 
+# Citation for Vader Sentiment Analysis
+# Hutto, C.J. & Gilbert, E.E. (2014). VADER: A Parsimonious Rule-based Model for
+# Sentiment Analysis of Social Media Text. Eighth International Conference on
+# Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
+
 def sentiment_matching(df, lst, year):
     tmp = pd.DataFrame()
     for item in lst:
@@ -36,10 +55,20 @@ def sentiment_matching(df, lst, year):
         sentiment = []
         subjectivity = []
         tempdf = df[df[item].notna()]
-        tempdf['sentiment_score'] = tempdf[item].apply(get_sentiment)
-        tempdf['sentiment_group'] = tempdf['sentiment_score'].apply(sentiment_groups)
+        # Using the Textblob Sentiment Analyser
+        tempdf['sentiment_score_textblob'] = tempdf[item].apply(get_sentiment)
+        tempdf['sentiment_group_textblob'] = tempdf['sentiment_score_textblob'].apply(sentiment_groups)
+        
+        # Using the Vader Sentiment Analyser
+        tempdf['sentiment_score_vader'] = tempdf[item].apply(get_vader_sentiment)
+        tempdf['sentiment_group_vader'] = tempdf['sentiment_score_vader'].apply(sentiment_groups)
+        
+        # Getting the subjectivity of the scores
         tempdf['subjectivity_score'] = tempdf[item].apply(get_subjectivity)
         tempdf['subjectivity_group'] = tempdf['subjectivity_score'].apply(subjectivity_groups)
+        
+        tempdf['sentiment_differences'] = tempdf.apply(lambda x: x['sentiment_group_textblob']==x['sentiment_group_vader'], axis=1)
+        
         tempdf['category_assessed'] = item
         tmp = pd.concat([tmp, tempdf])
     tmp.to_csv(f'data/processed/sentiment/sentiment_linked/{year}.csv')
@@ -49,3 +78,6 @@ sentiment_matching(df20, ['priorities and concerns','important learning','commen
 sentiment_matching(df21, ["Can you please tell us your organisations current key priorities and or concerns",
                                       'Can you tell us a short story about your experiences',
                                       'Is there anything else you would like to share with us or comment on'], 2021)
+
+
+
